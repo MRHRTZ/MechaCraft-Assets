e30f4fa4179b7f70f9b62ff451bf9bff4ec46d52
MRHRTZ <hanifsyauqi61@gmail.com>
2023-08-29 12:33:14 +0700

    prepare server-net module

--- scripts/libs/utils.ts
+++ scripts/libs/utils.ts
@@ -1,25 +1,5 @@
 import { world } from "@minecraft/server";
 import { faktaText } from "./fakta";
-// import * as mcnet from "@minecraft/server-net";
-
-// let authKey = "mechacraft-2023";
-
-// async function getRequest(url: string): Promise<mcnet.HttpResponse> {
-//     const req = new mcnet.HttpRequest(url);
-//     req.method = mcnet.HttpRequestMethod.GET;
-//     req.headers = [new mcnet.HttpHeader("Content-Type", "application/json"), new mcnet.HttpHeader("auth", authKey)];
-
-//     return await mcnet.http.request(req);
-// }
-
-// async function postRequest(url: string, data: any): Promise<mcnet.HttpResponse> {
-//     const req = new mcnet.HttpRequest(url);
-//     req.body = JSON.stringify(data);
-//     req.method = mcnet.HttpRequestMethod.POST;
-//     req.headers = [new mcnet.HttpHeader("Content-Type", "application/json"), new mcnet.HttpHeader("auth", authKey)];
-
-//     return await mcnet.http.request(req);
-// }
 
 class Notes {
     note = "";
@@ -434,8 +414,6 @@ function showErrorToOP(e: Error | any) {
 }
 
 export {
-    // getRequest,
-    // postRequest,
     Notes,
     viewObj,
     isPlayerExist,
